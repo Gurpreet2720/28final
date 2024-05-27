a924ee278d4ebf27c954cfde2e8eae56032c465b
Gurpreet2720 <gurpreet0535.be21@chitkara.edu.in>
2024-05-27 23:55:26 +0530

    ll

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ import mysql2 from "mysql2";
 export const db=mysql2.createConnection({
     host:"bm5cn65rleggg500ov1p-mysql.services.clever-cloud.com",
     user:"uxj9pvgqfhrx0tuw",
-    port:"3306";
+    port:"3306",
     password:"PTMSeLGbCfSy6hMPK42A",
     database:"bm5cn65rleggg500ov1p",
 })
